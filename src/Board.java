5ce95c61d7c01f8459452072e63d3b97bd69212e
DJSkyRoad <jakobpauls2@gmail.com>
2022-03-30 18:14:36 +0200

    board

---
+++ src/Board.java
@@ -0,0 +1,15 @@
+public class Board {
+    public String toString() {
+        String brdStr = "";
+        brdStr += "  a b c d e f g h\n";
+        for (int r = 0; r < 8; r++) {
+            brdStr += (8 - r) + "";
+            for (int c = 0; c < 8; c++) {
+                brdStr += " .";
+            }
+            brdStr += " " + (8 - r) + "\n";
+        }
+        brdStr += "  a b c d e f g h\n";
+        return brdStr;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	
+        Board brd = new Board();
+        System.out.println(brd);
     }
 }
