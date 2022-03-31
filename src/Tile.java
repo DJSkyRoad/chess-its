dc5489c4e021786a58eb420bb7a8d07be934e427
DJSkyRoad <jakobpauls2@gmail.com>
2022-03-31 18:13:20 +0200

    position update

--- src/Tile.java
+++ src/Tile.java
@@ -4,7 +4,16 @@ public class Tile {
     private final int y;
     public static final int width = 5;
     public static final int height = 3;
-    public static final String startPos = "";
+    public static String[][] pos = {
+            {"r", "n", "b", "q", "k", "b", "n", "r"},
+            {"p", "p", "p", "p", "p", "p", "p", "p"},
+            {".",".",".",".",".",".",".","."},
+            {".",".",".",".",".",".",".","."},
+            {".",".",".",".",".",".",".","."},
+            {".",".",".",".",".",".",".","."},
+            {"p", "p", "p", "p", "p", "p", "p", "p"},
+            {"r", "n", "b", "q", "k", "b", "n", "r"}};
+
 
     public Tile(int x, int y) {
         this.x = x;
@@ -27,34 +36,29 @@ public class Tile {
                     else if (this.x == 0) s += '\u251C';
                     else if (this.y == 0) s += '\u252C';
                     else s += '\u253C';
-                }
-                else if (i == width - 1) {
+                } else if (i == width - 1) {
                     if (this.x == Board.scale - 1 && this.y == 0) s += '\u2510';
                     else if (this.x == Board.scale - 1) s += '\u2524';
                     else if (this.y == 0) s += '\u252C';
                     else s += '\u253C';
-                }
-                else s += '\u2500';
-            }
-            else if (line == height - 1) {
+                } else s += '\u2500';
+            } else if (line == height - 1) {
                 if (i == 0) {
                     if (this.x == 0 && this.y == Board.scale - 1) s += '\u2514';
                     else if (this.x == 0) s += '\u251C';
                     else if (this.y == Board.scale - 1) s += '\u2534';
                     else s += '\u253C';
-                }
-                else if (i == width - 1) {
+                } else if (i == width - 1) {
                     if (this.x == Board.scale - 1 && this.y == Board.scale - 1) s += '\u2518';
                     else if (this.x == Board.scale - 1) s += '\u2524';
                     else if (this.y == Board.scale - 1) s += '\u2534';
                     else s += '\u253C';
-                }
-                else s += '\u2500';
-            }
-            else {
+                } else s += '\u2500';
+            } else {
                 if (i == 0 || i == width - 1) s += '\u2502';
-                else if (this.x == 0 && this.y == 1) s += '\u265C';
-                else s += " ";
+                else if (pos[this.y][this.x] == ".") s += " ";
+                else s += pos[this.y][this.x];
+
             }
         }
         if (this.x == Board.scale - 1) {
