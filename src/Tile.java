d5bfaa3b2f43e4fcfaad2ecdc49c66a7a33ea5ea
Skullman-G <skullmanslashgames@gmail.com>
2022-03-31 19:08:02 +0200

    fixed chess piece placement

--- src/Tile.java
+++ src/Tile.java
@@ -4,6 +4,19 @@ public class Tile {
     private final int y;
     public static final int width = 5;
     public static final int height = 3;
+
+    public final char r = '\u265C';
+    public final char n = '\u265E';
+    public final char b = '\u265D';
+    public final char q = '\u265B';
+    public final char k = '\u265A';
+    public final char p = '\u265F';
+    public final char R = '\u2656';
+    public final char N = '\u2658';
+    public final char B = '\u2657';
+    public final char Q = '\u2655';
+    public final char K = '\u2654';
+    public final char P = '\u2659';
     public static String[][] pos = {
             {"r", "n", "b", "q", "k", "b", "n", "r"},
             {"p", "p", "p", "p", "p", "p", "p", "p"},
@@ -23,47 +36,46 @@ public class Tile {
     public String toString(int line) {
         if (line >= height || line < 0) return "INVALID LINE INDEX";
         String s = "";
+        if (line == 0 && this.y > 0) return s;
         if (this.x == 0) {
-            if (line == height / 2) s += s += (Board.scale - this.y) + " ";
+            if (line == height / 2) s += (Board.scale - this.y) + " ";
             else s += "  ";
         }
-        for (int i = 0; i < width; i++) {
-            if (i == 0 && this.x > 0) continue;
+        for (int column = 0; column < width; column++) {
+            if (column == 0 && this.x > 0) continue;
             if (line == 0) {
-                if (this.y > 0) return s;
-                if (i == 0) {
+                if (column == 0) {
                     if (this.x == 0 && this.y == 0) s += '\u250C';
                     else if (this.x == 0) s += '\u251C';
                     else if (this.y == 0) s += '\u252C';
                     else s += '\u253C';
-                } else if (i == width - 1) {
+                } else if (column == width - 1) {
                     if (this.x == Board.scale - 1 && this.y == 0) s += '\u2510';
                     else if (this.x == Board.scale - 1) s += '\u2524';
                     else if (this.y == 0) s += '\u252C';
                     else s += '\u253C';
                 } else s += '\u2500';
             } else if (line == height - 1) {
-                if (i == 0) {
+                if (column == 0) {
                     if (this.x == 0 && this.y == Board.scale - 1) s += '\u2514';
                     else if (this.x == 0) s += '\u251C';
                     else if (this.y == Board.scale - 1) s += '\u2534';
                     else s += '\u253C';
-                } else if (i == width - 1) {
+                } else if (column == width - 1) {
                     if (this.x == Board.scale - 1 && this.y == Board.scale - 1) s += '\u2518';
                     else if (this.x == Board.scale - 1) s += '\u2524';
                     else if (this.y == Board.scale - 1) s += '\u2534';
                     else s += '\u253C';
                 } else s += '\u2500';
             } else {
-                if (i == 0 || i == width - 1) s += '\u2502';
-                else if (pos[this.y][this.x] == ".") s += " ";
-                else s += pos[this.y][this.x];
-
+                if (column == 0 || column == width - 1) s += '\u2502';
+                else if (column == width / 2 && pos[this.y][this.x] != ".") s += pos[this.y][this.x];
+                else s += " ";
             }
         }
         if (this.x == Board.scale - 1) {
-            if (line == height / 2) s += s += " " + (Board.scale - this.y) + '\n';
-            else s += '\n';
+            if (line == height / 2) s += " " + (Board.scale - this.y);
+            s += '\n';
         }
         return s;
     }
