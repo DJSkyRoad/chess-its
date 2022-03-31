d2d9810801ec3ba4a20396461324972f480afbaa
Jakob <77412019+DJSkyRoad@users.noreply.github.com>
2022-03-31 22:47:38 +0200

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Board brd = new Board();
         System.out.println(brd);
+        
+        String inp = scanner.nextLine();
+        System.out.println(inp);
     }
 }
