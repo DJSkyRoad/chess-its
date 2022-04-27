98244ecdfbea010c8babca31cc32fd4f06eac3dc
Skullman-G <skullmanslashgames@gmail.com>
2022-04-27 14:38:14 +0200

    changed window icon

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,25 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
     	JFrame window = new JFrame();
     	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setTitle("Chess");
+
+		BufferedImage icon = null;
+		try {
+			icon = ImageIO.read(Main.class
+					.getResourceAsStream("/resources/black_king.png"));
+			window.setIconImage(icon);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		window.setResizable(false);
 
     	GamePanel panel = new GamePanel();
