02cf5cc2e1bcb6740984be93d9b6e9c7ff0aeb5a
MattisWeigold <105352936+MattisWeigold@users.noreply.github.com>
2022-06-01 08:12:10 +0200

    added multimove

---
+++ src/main/math/MultiMove.java
@@ -0,0 +1,31 @@
+package main.math;
+
+import main.pieces.ChessPiece;
+
+public class MultiMove extends Move {
+    public final ChessPos pos2;
+    public final ChessPos dest2;
+
+    public MultiMove(ChessPos pos, ChessPos dest, ChessPos pos2, ChessPos dest2) {
+        super(pos, dest);
+        this.pos2 = pos2;
+        this.dest2 = dest2;
+    }
+
+    public ChessPiece[][] getTestPosCopy(ChessPiece[][] original) {
+        ChessPiece[][] test = new ChessPiece[original.length][original.length];
+        for (int i = 0; i < original.length; i++) {
+            System.arraycopy(original[i], 0, test[i], 0, original.length);
+        }
+
+        ChessPiece piece = test[this.pos.y][this.pos.x];
+        test[this.pos.y][this.pos.x] = null;
+        test[this.dest.y][this.dest.x] = piece;
+
+        ChessPiece piece2 = test[this.pos2.y][this.pos2.x];
+        test[this.pos2.y][this.pos2.x] = null;
+        test[this.dest2.y][this.dest2.x] = piece2;
+
+        return test;
+    }
+}
