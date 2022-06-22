ad3e484afdfd1c500282aedea9ed748f163137ae
ventolotl <ventolotl@protonmail.com>
2022-06-22 16:12:31 +0200

    remove duplicated code

--- src/main/pieces/Rook.java
+++ src/main/pieces/Rook.java
@@ -4,20 +4,11 @@ import main.math.ChessPos;
 import main.math.Move;
 import main.scenes.GameScene;
 
-import javax.imageio.ImageIO;
-import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
 public class Rook extends ChessPiece {
     public Rook(GameScene.Faction faction) {
-        super(faction);
-        try {
-            this.image = ImageIO.read(Objects.requireNonNull(getClass()
-                    .getResourceAsStream(faction.isWhite() ? "/resources/white_rook.png" : "/resources/black_rook.png")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        super(faction, "rook");
     }
 
     @Override
