c1984057d3b4415d1cbfb79ca146b2436e3e5b0c
Skullman-G <skullmanslashgames@gmail.com>
2022-06-22 17:07:51 +0200

    Update JoinMenuScene.java

--- src/main/scenes/JoinMenuScene.java
+++ src/main/scenes/JoinMenuScene.java
@@ -17,7 +17,14 @@ public class JoinMenuScene extends Scene {
         this.addWidget(this.ip);
         this.addWidget(this.port);
         this.addWidget(new Button("Join", Game.panelSize / 2, Game.panelSize / 2 + 120, 200, 50, (button) -> {
-            Game.INSTANCE.startClient(this.ip.getContent(), Integer.getInteger(this.port.getContent()));
+            int p;
+            try {
+                p = Integer.parseInt(this.port.getContent());
+            }
+            catch (NumberFormatException e) {
+                p = 1234;
+            }
+            Game.INSTANCE.startClient(this.ip.getContent(), p);
             Game.INSTANCE.setScene(new LoadingScene("Waiting for Opponent...", () -> {
                 Game.INSTANCE.getConnection().ifPresent((c) -> {
                     Game.INSTANCE.setScene(new GuestMenuScene());
