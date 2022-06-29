7ef46e7c43575e454bc448cc96e3407a333a98ec
Jakob <77412019+DJSkyRoad@users.noreply.github.com>
2022-06-29 08:32:54 +0200

    Format

--- src/main/pieces/Pawn.java
+++ src/main/pieces/Pawn.java
@@ -24,23 +24,39 @@ public class Pawn extends ChessPiece {
 
         if (this.getFaction().isWhite()) {
             ChessPos dest = pos.add(0, -1);
-            if (dest.isValid() && board[dest.y][dest.x] == null) moves.add(new Move(pos, dest));
+            if (dest.isValid() && board[dest.y][dest.x] == null) {
+                moves.add(new Move(pos, dest));
+            }
             dest = pos.add(1, -1);
-            if (dest.isValid() && board[dest.y][dest.x] != null && !board[dest.y][dest.x].getFaction().isWhite()) moves.add(new Move(pos, dest));
+            if (dest.isValid() && board[dest.y][dest.x] != null && !board[dest.y][dest.x].getFaction().isWhite()) {
+                moves.add(new Move(pos, dest));
+            }
             dest = pos.add(-1, -1);
-            if (dest.isValid() && board[dest.y][dest.x] != null && !board[dest.y][dest.x].getFaction().isWhite()) moves.add(new Move(pos, dest));
+            if (dest.isValid() && board[dest.y][dest.x] != null && !board[dest.y][dest.x].getFaction().isWhite()) {
+                moves.add(new Move(pos, dest));
+            }
             dest = pos.add(0, -2);
-            if (dest.isValid() && board[dest.y][dest.x] == null && pos.y == Board.scale - 2) moves.add(new Move(pos, dest));
+            if (dest.isValid() && board[dest.y][dest.x] == null && pos.y == Board.scale - 2) {
+                moves.add(new Move(pos, dest));
+            }
         }
         else {
             ChessPos dest = pos.add(0, 1);
-            if (dest.isValid() && board[dest.y][dest.x] == null) moves.add(new Move(pos, dest));
+            if (dest.isValid() && board[dest.y][dest.x] == null) {
+                moves.add(new Move(pos, dest));
+            }
             dest = pos.add(1, 1);
-            if (dest.isValid() && board[dest.y][dest.x] != null && board[dest.y][dest.x].getFaction().isWhite()) moves.add(new Move(pos, dest));
+            if (dest.isValid() && board[dest.y][dest.x] != null && board[dest.y][dest.x].getFaction().isWhite()) {
+                moves.add(new Move(pos, dest));
+            }
             dest = pos.add(-1, 1);
-            if (dest.isValid() && board[dest.y][dest.x] != null && board[dest.y][dest.x].getFaction().isWhite()) moves.add(new Move(pos, dest));
+            if (dest.isValid() && board[dest.y][dest.x] != null && board[dest.y][dest.x].getFaction().isWhite()) {
+                moves.add(new Move(pos, dest));
+            }
             dest = pos.add(0, 2);
-            if (dest.isValid() && board[dest.y][dest.x] == null && pos.y == 1) moves.add(new Move(pos, dest));
+            if (dest.isValid() && board[dest.y][dest.x] == null && pos.y == 1) {
+                moves.add(new Move(pos, dest));
+            }
         }
 
         return moves;
